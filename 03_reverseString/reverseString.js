d8a8b07b5c57f71e7eee2db1983f4cea521cfd93
Gabriel Simonassi <gabrielsimonassi@id.uff.br>
2023-02-24 23:35:31 -0300

    Escreve uma string ao contrario na saida padrao

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	let len = word.length - 1; 
+	word = word.split("");
+	let newWord = "";
 
+	for(len; len >= 0; len--){
+	newWord += word[len];
+}
+	return(newWord);
 };
 
 // Do not edit below this line
