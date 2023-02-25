49847c0042bd4ee864678d0a27267531d87ad063
Gabriel Simonassi <gabrielsimonassi@id.uff.br>
2023-02-24 23:36:59 -0300

    Indica se um ano passado pelo usuario é bissexto ou não

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(ano) {
+	if (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0)) return true;
+	else return false;
 };
 
 // Do not edit below this line
