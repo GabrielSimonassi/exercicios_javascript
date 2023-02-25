9ce4cd6635e7eecb4aef155be85d3ed577b4db26
Gabriel Simonassi <gabrielsimonassi@id.uff.br>
2023-02-24 23:36:35 -0300

    Soma todos os numeros entre dois inteiros maiores que 0 indicados pelo usuario

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if (a < 0 || b < 0 || !(typeof (a) == typeof(b))) return "ERROR";
+	let min;
+	let max;
+	let res = 0;
 
+	if (a > b){
+		max = a;
+		min = b;
+	}
+	else if (a < b){
+		min = a;
+		max = b;
+	}
+
+	for (let i = min; i <= max; i++){
+		res += i;
+	}
+	return (res);
 };
 
 // Do not edit below this line
